43104b273bc3cbe17e5ed634c21093b9b90e47e1
eghrhegpe <134900655+eghrhegpe@users.noreply.github.com>
2026-05-30 01:26:15 +0800

    a

--- list.bat
+++ list.bat
@@ -1,11 +1,13 @@
 @echo off
-set OUTPUT=OnlyNames.txt
+chcp 65001 >nul
+set OUT=YSM_ZIP_7Z_List.txt
 
-REM 使用 /s 递归，但只输出文件名（因为 dir 在递归时如果不加 /b 会显示路径，这里用 for 提取单纯文件名）
-for /r %%i in (*) do (
-    echo %%~nxi >> %OUTPUT%
+> "%OUT%" echo ===== YSM / ZIP / 7Z 文件清单 =====
+
+for /r %%i in (*.ysm *.zip *.7z) do (
+    echo %%~nxi >> "%OUT%"
 )
 
-echo 已完成！仅文件名列表已生成：%OUTPUT%
-start "" %OUTPUT%
+echo 已完成！共提取了以下文件：
+type "%OUT%"
 pause
